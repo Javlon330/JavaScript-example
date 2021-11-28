79b584c76f1312d395c1e8ffda548ef96bb8ecbe
Javlon330 <92207619+Javlon330@users.noreply.github.com>
2021-11-28 14:52:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,3 +20,25 @@ let rawString = "aaabbbccddd";
 console.log(`Raw: ${rawString}`);
 const compres = Compression(rawString);
 console.log(`Compressed: ${compres}`);
+
+/* ==========  isVisitAllRooms  ==========*/
+
+const visitAllRooms = (rooms) => {
+    let keys = new Set(rooms[0]);
+    for (let key of keys)
+        for (let room of rooms[key])
+            keys.add(room);
+
+    let keySize = keys.size ;
+    if (!keys.has(0)) keySize++;
+
+    console.log(rooms.length);
+    console.log(keySize);
+
+    return rooms.length === keySize;
+};
+
+console.log(visitAllRooms([[1], [2], [3], []]))  //true
+
+console.log(visitAllRooms([[1,3], [3,0,1], [2], [0]])) //false
+console.log(visitAllRooms([[3], [2], [], [1]])); //true
